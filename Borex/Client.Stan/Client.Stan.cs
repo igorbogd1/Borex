069d172d1c09f758f676226f8f35fda5c9c240d9
Богданов Игорь Владимирович <igor_bogd@mail.ru>
2021-10-16 00:10:02 +0500

    Добавлена конверсия валют по стратегии Стэна

---
+++ Borex/Client.Stan/Client.Stan.cs
@@ -0,0 +1,24 @@
+﻿using Borexx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Client.Stan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            account[Currencies.USD] = account[Currencies.EUR] =
+                account[Currencies.CZH] = account[Currencies.PLN] = 100;
+            var server = new BorexServer();
+            var currencies = server
+                                    .Rates
+                                    .OrderBy(z => z.RelativeGrowth)
+                                    .Select(z => z.Currency)
+                                    .ToArray();
+            server.Exchange(account, currencies.First(), currencies.Last(), 100);
+        }
+    }
+}
