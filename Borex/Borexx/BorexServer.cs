446a91ab66cbce45c5680c78a51ca9d0f8c0fa55
Богданов Игорь Владимирович <igor_bogd@mail.ru>
2021-10-15 12:17:57 +0500

    Добавлено логирование

--- Borex/Borexx/BorexServer.cs
+++ Borex/Borexx/BorexServer.cs
@@ -25,6 +25,7 @@ namespace Borexx
         }
         public void Exchange(Account account, Currencies from, Currencies to, double amount)
         {
+            Console.WriteLine("{0,7:0.00} transferred from {1,-5} to {2,-5}", amount, from, to);
             account[from] -= amount;
             amount *= Rates.Where(z => z.Currency == from).FirstOrDefault().Cost;
             amount *= 0.95;
